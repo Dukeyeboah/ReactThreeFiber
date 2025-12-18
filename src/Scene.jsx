086dddc891e3030c966f304a5b6e00de5cb81ff1
Duke <dukeyeboah@proton.me>
2025-12-18 22:31:54 +0000

    datGUI leva & perf

--- src/Scene.jsx
+++ src/Scene.jsx
@@ -1,15 +1,45 @@
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls, TransformControls } from '@react-three/drei';
+import {
+  OrbitControls,
+  TransformControls,
+  PivotControls,
+  Float,
+  MeshReflectorMaterial,
+} from '@react-three/drei';
 import Sphere from './components/Sphere';
 import Box from './components/Box';
 import Floor from './components/Floor';
 import CustomObject from './components/CustomObject';
 // import * as THREE from 'three';
 import { useRef } from 'react';
+import { Mesh } from 'three';
+import { useControls, button } from 'leva';
+import { Perf } from 'r3f-perf';
 
 export default function Scene() {
   // const cameraRef = useRef();
   const sphereRef = useRef();
+  //   const controls = useControls({
+  //     position: -2,
+  //   })
+
+  const { perfVisible } = useControls({ perfVisible: false });
+  const { position, color, visible } = useControls('sphere folder', {
+    visible: true,
+    position: {
+      value: { x: -2, y: 1 },
+      min: -4,
+      max: 5,
+      step: 0.01,
+      joystick: 'invertY',
+    },
+    color: 'green',
+    choice: { options: ['red', 'green', 'blue'] },
+    clickMe: button(() => {
+      console.log('button clicked');
+    }),
+    //options:['#111111', '#222222', '#333333']
+  });
 
   return (
     <Canvas
@@ -28,15 +58,56 @@ export default function Scene() {
 
       <Sphere position={[-1.75, 0, 0]} />
 
-      <mesh ref={sphereRef} position={[-0.75, -2, 0]} scale={0.5}>
+      <mesh
+        ref={sphereRef}
+        position={[position.x, position.y, 0]}
+        scale={0.5}
+        visible={visible}
+      >
         <sphereGeometry args={[1, 32, 32]} />
-        <meshStandardMaterial color='#111111' wireframe />
+        <meshStandardMaterial color={color} wireframe />
       </mesh>
-      <TransformControls object={sphereRef} />
+      {/* <TransformControls object={sphereRef} mode='translate' /> */}
       <Box position={[1.75, 0, 0]} />
       {/* <CustomObject/> */}
       {/* <Floor position={[0, -1.5, 0]} /> */}
+      {/* <PivotControls
+        anchor={[0, 0, 0]}
+        depthTest={false}
+        lineWidth={4}
+        axisColors={['#9381ff', '#ff4d6d', '#7ae582']}
+        scale={100}
+        fixed={true}
+        hidden={true}
+      >
+        <Float speed={5} floatIntensity={2}>
+          <mesh position-x={-3} scale={0.3}>
+            <sphereGeometry />
+            <meshStandardMaterial color='orange' />
+          </mesh>
+        </Float>
+      </PivotControls> */}
+      <mesh position={[0, -1.5, 0]} scale={[200, 1, 200]}>
+        <boxGeometry args={[1, 1, 1]} />
+        {/* <meshStandardMaterial color='#888888' wireframe /> */}
+        {/* <meshStandardMaterial color='pink' /> */}
+        <MeshReflectorMaterial
+          mirror={0.5}
+          color='white'
+          //   metalness={0.5}
+          //   roughness={0.5}
+          blur={[1000, 500]}
+          mixBlur={30}
+          //   mixStrength={60}
+          //   mixContrast={1}
+          //   resolution={1024}
+          //   depthScale={0.01}
+          //   minDepthThreshold={0.9}
+          //   maxDepthThreshold={1}
+        />
+      </mesh>
 
+      {perfVisible && <Perf position='top-left' />}
       <OrbitControls makeDefault enableDamping dampingFactor={0.05} />
     </Canvas>
   );
